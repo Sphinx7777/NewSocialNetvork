9454ef54fa56265b4cb7400f82d2e82feac54854
Sphinx7777 <spamoglot13@gmail.com>
2019-09-05 08:48:32 +0300

    Refactor

---
+++ src/Components/Users/PrevNextTopBtn.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import s from './Users.module.scss';
+
+
+export const PrevNextTopBtn = (props) => {
+	let {
+		currentPage, onClickNumberOfPage, pages,usersScroll
+	} = props;
+
+
+
+	let toTheTop = () => {
+		usersScroll.current.scrollTop = 0;
+	};
+
+	return (
+		<div className={s.showPages}>
+			<button onClick={() => {
+				onClickNumberOfPage(currentPage > 1 ? currentPage - 1 : currentPage);
+				toTheTop();
+			}} className={s.showPrevPage} disabled={currentPage === 1}>Show prev page ↩
+			</button>
+			<button className={s.toTheTop} onClick={() => {
+				toTheTop()
+			}}>To the top
+			</button>
+			<button onClick={() => {
+				onClickNumberOfPage(currentPage < pages.length ? currentPage + 1 : currentPage);
+				toTheTop();
+			}} className={s.showNextPage} disabled={currentPage === pages.length}>↪ Show next page
+			</button>
+		</div>
+	);
+};
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,6 +3,7 @@ import s from './Users.module.scss';
 import {User} from "./User";
 import {NumberUsersOfPage} from "./NumberUsersOfPage";
 import {Search} from "./Search";
+import {PrevNextTopBtn} from "./PrevNextTopBtn";
 
 
 export const Users = (props) => {
@@ -35,10 +36,6 @@ export const Users = (props) => {
 
 	let usersScroll = React.createRef();
 
-	let toTheTop = () => {
-		usersScroll.current.scrollTop = 0;
-	};
-
 	return (
 		<div className={s.users} ref={usersScroll}>
 			<div className={s.stringPages}>
@@ -64,23 +61,10 @@ export const Users = (props) => {
 			</div>
 			<User users={users}
 						loadLogin={loadLogin}/>
-
-			<div className={s.showPages}>
-				<button onClick={() => {
-					onClickNumberOfPage(currentPage > 1 ? currentPage - 1 : currentPage);
-					toTheTop();
-				}} className={s.showPrevPage} disabled={currentPage === 1}>Show prev page ↩
-				</button>
-				<button className={s.toTheTop} onClick={() => {
-					toTheTop()
-				}}>To the top
-				</button>
-				<button onClick={() => {
-					onClickNumberOfPage(currentPage < pages.length ? currentPage + 1 : currentPage);
-					toTheTop();
-				}} className={s.showNextPage} disabled={currentPage === pages.length}>↪ Show next page
-				</button>
-			</div>
+			<PrevNextTopBtn usersScroll={usersScroll}
+											currentPage={currentPage}
+											onClickNumberOfPage={onClickNumberOfPage}
+											pages={pages}/>
 		</div>
 
 	);
