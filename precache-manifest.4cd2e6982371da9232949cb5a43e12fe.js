6dc9c6aa830e5f57f6bb2124dce8c7357c5ffdd6
Sphinx7777 <spamoglot13@gmail.com>
2019-09-15 01:13:36 +0300

    Updates

---
+++ precache-manifest.4cd2e6982371da9232949cb5a43e12fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b899e85877411662f020a8fa128342",
+    "url": "/SocialNetvork/index.html"
+  },
+  {
+    "revision": "f5bc62a02c2f10abaad0",
+    "url": "/SocialNetvork/static/css/main.1e5a5ec8.chunk.css"
+  },
+  {
+    "revision": "69d17917a346d181c78f",
+    "url": "/SocialNetvork/static/js/2.d328a85f.chunk.js"
+  },
+  {
+    "revision": "f5bc62a02c2f10abaad0",
+    "url": "/SocialNetvork/static/js/main.5086f6e2.chunk.js"
+  },
+  {
+    "revision": "30e51b4df2a60b0a69cc",
+    "url": "/SocialNetvork/static/js/runtime~main.d65f8260.js"
+  },
+  {
+    "revision": "fb6b8e6013ad8ed9e0788759373f0663",
+    "url": "/SocialNetvork/static/media/forest.fb6b8e60.jpg"
+  },
+  {
+    "revision": "bd1115345bbb7c8bd8b2c50fc46efeb9",
+    "url": "/SocialNetvork/static/media/forest3.bd111534.jpg"
+  },
+  {
+    "revision": "56cca69f99ed01f260b55078015e258c",
+    "url": "/SocialNetvork/static/media/forest4.56cca69f.jpg"
+  },
+  {
+    "revision": "2bcad980cf95fcdfb7337205ea8612d5",
+    "url": "/SocialNetvork/static/media/git.2bcad980.ico"
+  },
+  {
+    "revision": "b4166a32ba33162329d4e62f2ecbed69",
+    "url": "/SocialNetvork/static/media/lamp.b4166a32.ico"
+  },
+  {
+    "revision": "a61a1279a68ef99ae4bf7f7c77024eef",
+    "url": "/SocialNetvork/static/media/skull2.a61a1279.png"
+  },
+  {
+    "revision": "5e18d5208289f562929dd840976ccba3",
+    "url": "/SocialNetvork/static/media/telegram.5e18d520.ico"
+  }
+]);
