97e9aa2e52d708608093cfb8f774d1b38b25d15b
Sphinx7777 <spamoglot13@gmail.com>
2019-09-19 01:33:22 +0300

    PC

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -10,30 +10,32 @@ import {StringOfPage} from "../Others/StringOfPage/StringOfPage";
 export const Users = (props) => {
 	let {
 		totalNumberOfUsers, numberUsersOnPage, currentPage,
-		onClickNumberOfPage,searchUsers, users, loadLogin,SetNumberUsersOnPageAC,
+		onClickNumberOfPage, searchUsers, users, loadLogin, SetNumberUsersOnPageAC,
 	} = props;
 
 
 	return (
 		<React.Fragment>
-		<div className={s.users}>
-			<div className={s.stringPages}>
-				<NumberUsersOfPage {...{currentPage,totalNumberOfUsers,
-					SetNumberUsersOnPageAC,numberUsersOnPage}}/>
+			<div className={s.users}>
+				<div className={s.stringPages}>
+					<NumberUsersOfPage {...{
+						currentPage, totalNumberOfUsers,
+						SetNumberUsersOnPageAC, numberUsersOnPage
+					}}/>
 
-				<StringOfPage {...{
-					onClickNumberOfPage, currentPage,
-					totalNumberOfUsers, numberUsersOnPage
-				}}/>
+					<StringOfPage {...{
+						onClickNumberOfPage, currentPage,
+						totalNumberOfUsers, numberUsersOnPage
+					}}/>
 
-				<Search {...{searchUsers}}/>
+					<Search {...{searchUsers}}/>
+				</div>
+				<div>
+					<User users={users} loadLogin={loadLogin}/>
+
+				</div>
+
 			</div>
-			<div>
-				<User users={users} loadLogin={loadLogin}/>
-
-			</div>
-
-		</div>
 			<PrevNextTopBtn {...{
 				currentPage, onClickNumberOfPage,
 				totalNumberOfUsers, numberUsersOnPage
